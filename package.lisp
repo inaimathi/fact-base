e768c56efdedfac8409fa27b774cd91996188f18
inaimathi <inaimathi@self>
2014-03-20 09:38:47 -0400

    -Rejigged the package.lisp file

--- package.lisp
+++ package.lisp
@@ -5,8 +5,9 @@
   (:shadow #:delete)
   (:import-from #:alexandria #:with-gensyms)
   (:import-from #:anaphora #:awhen #:aif #:it)
-  (:export :make-fact-base :matching? :current :history :delta :_
+  (:export :make-fact-base :current :history :delta :_
 	   :file-name :next-id! 
-	   :select :lookup :insert! :multi-insert! :delete! :project! :project
-	   :write! :write-delta! :load! :index!))
+	   :lookup :select :matching? 
+	   :multi-insert! :insert! :index! :delete! :project! :project
+	   :write! :write-delta! :load!))
 
