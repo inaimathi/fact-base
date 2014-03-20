e7449f6b1451352a9c89f740fb38e8619fc6e45a
inaimathi <inaimathi@self>
2014-03-20 09:30:24 -0400

    -The delta is now kept explicitly.      - This makes delta writes much faster than they used to be, but they use more memory      - fact-base now exports :delta instead of :last-saved      - insert! on a fact-base now adds an entry to both :history and :delta      - still need to do a bit of cleanup around read!

--- package.lisp
+++ package.lisp
@@ -5,7 +5,7 @@
   (:shadow #:delete)
   (:import-from #:alexandria #:with-gensyms)
   (:import-from #:anaphora #:awhen #:aif #:it)
-  (:export :make-fact-base :matching? :current :history :last-saved :_
+  (:export :make-fact-base :matching? :current :history :delta :_
 	   :file-name :next-id! 
 	   :select :lookup :insert! :multi-insert! :delete! :project! :project
 	   :write! :write-delta! :load! :index!))
