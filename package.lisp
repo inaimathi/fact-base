3d472794aaba41c3842825bb3f69e8e6ba4ee90e
inaimathi <leo.zovic@gmail.com>
2018-02-17 01:10:09 -0500

    Expose tag! and tags-of
    - Add tag! and tags-of to exported symbols list in package.lisp

--- package.lisp
+++ package.lisp
@@ -5,7 +5,7 @@
   (:shadow #:delete)
   (:import-from #:alexandria #:with-gensyms #:if-let #:when-let)
   (:import-from #:local-time #:timestamp)
-  (:export #:fact-base #:make-fact-base #:current #:delta #:total-entries #:fork-at #:rewind-by #:rewind-to
+  (:export #:fact-base #:make-fact-base #:current #:delta #:total-entries #:fork-at #:rewind-by #:rewind-to #:tag! #:tags-of
 	   #:file-name #:next-id!
 	   #:for-all #:lookup
 	   #:multi-insert! #:insert-new! #:insert-if-unique! #:insert! #:delete! #:change!
